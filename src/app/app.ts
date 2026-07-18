cfa3efb08e38c060a1f3009228438187c3f11c98
jovana vieira miranda <jovanavieira@gmail.com>
2026-07-17 21:23:07 -0300

    refactor: atualiza arquivos principais da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //remove a importação de RouterOutlet, pois não é necessario no momento;
-import {Produto} from './components/produto/produto';//importando a classe do produto do arquivo produto.ts para produto/produto
+//import {Produto} from './components/produto/produto';//importando a classe do produto do arquivo produto.ts para produto/produto
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
