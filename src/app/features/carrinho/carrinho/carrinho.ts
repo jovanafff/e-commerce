15645f48760c0067581f29d1ae999f657f42bd75
jovana vieira miranda <jovanavieira@gmail.com>
2026-07-21 19:37:22 -0300

    feat: adiciona a funcionaliade do carrinho

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //! lógica de carrinho virá na proximas aulas
+  //! por ora, apenas exibe a página carrinho
+}
