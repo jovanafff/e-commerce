cddc28f535a9e2b8560a10b45c66c0d8639e9c2b
jovana vieira miranda <jovanavieira802@gmail>
2026-08-03 21:31:38 -0300

    feat: adicionar serviço de produtos

---
+++ src/app/features/produtos/produtos service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from "@angular/core";
+import { inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+type ProdutoApi = {
+    title: string;
+    price: number;
+};
+
+type Produto = {
+    nome: string;
+    preco: number;
+
+
+};
+
+@Injectable({ providedIn: 'root'})
+export class produtosService{
+    private http = inject(HttpClient);
+    private API = 'https://fakestoreapi.com/products';
+    buscarProdutos(){
+        return this.http.get<ProdutoApi[]>(this.API);
+    }
+    transFormarProdutos(dados: ProdutoApi[]): Produto[]{
+        return dados.map((p) =>({
+            nome: p.title,
+            preco: p.price,
+        }));
+    }
+}
