1879022e76b60851bc0c861201c7a3c8bb29b9e1
jovana vieira miranda <jovanavieira802@gmail>
2026-08-03 21:10:02 -0300

    chore:atualizar configuração da aplicação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,14 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(),
-  provideHttpClient(withFetch())
+  provideHttpClient(withFetch(),
+withInterceptors([httpInterceptor])),
   ]
 };
