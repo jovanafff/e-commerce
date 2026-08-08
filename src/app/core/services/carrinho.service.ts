d296f01cc9c3bd715eb7ffda26af74793d87bb5c
jovana vieira miranda <jovanavieira802@gmail>
2026-08-07 21:20:31 -0300

    feat (core): adicionar novos serviços

---
+++ src/app/core/services/carrinho.service.ts
@@ -0,0 +1,25 @@
+import { Injectable,signal, computed } from "@angular/core";
+
+@Injectable ({
+    providedIn: 'root'
+})
+
+export class CarrinhoService {
+//! Estado Global - Criado com Sucesso
+private carrinho = signal<{nome: string; preco: number}[]>([]);
+
+//? seleção
+itens = computed(() => this.carrinho());
+quantidadeItens = computed(()=> this.carrinho().length);
+totalItens = computed(() =>
+    this.carrinho().reduce((total, item) => total + item.preco,0));
+
+// TODA: Ação Adicionar Produtos
+adicionar(produto:{nome: string, preco: number}){
+    this.carrinho.update(lista => [...lista, produto]);
+}
+// TODO: Ação de Limpeza
+limpar() {
+    this.carrinho.set([]);
+}
+}
