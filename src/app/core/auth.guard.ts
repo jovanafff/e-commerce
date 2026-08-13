3e363c4c2b4681ffd25fc14bd0cab671538d6773
jovana vieira miranda <jovanavieira802@gmail>
2026-08-12 21:30:48 -0300

    refactor(auth): atualiza guard de autenticação

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,16 @@
 import { CanActivateFn } from '@angular/router';
-import { usuarioLogado} from './auth';
+import { AuthService } from './services/auth.service';
+import { inject } from '@angular/core';
+import { Router } from "@angular/router"
 
-export const authGuard: CanActivateFn = () => {
-    return usuarioLogado();
+
+
+export const authGuard: CanActivateFn = () =>{
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    if(authService.usuarioLogado()){
+        return true;
+    }
+    return router.createUrlTree(['/login']);
 };
