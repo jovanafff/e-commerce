779a797a0fc834dcaea845c5d597e35454c23699
jovana vieira miranda <jovanavieira802@gmail>
2026-08-19 21:55:24 -0300

    Atualiza carrinho.facade

--- src/app/core/facades/carrinho.facade.ts
+++ src/app/core/facades/carrinho.facade.ts
@@ -1,10 +1,8 @@
 import {Injectable, inject} from "@angular/core"
 import { CarrinhoService } from "../services/carrinho.service"
+import { ItemCarrinho } from "../models/item-carrinho";
 
-type ItemCarrinho = {
-    nome: string;
-    preco: number;
-}
+
 
 @Injectable({providedIn: 'root'})
 
@@ -23,6 +21,10 @@ export class CarrinhoFacade {
 
     limparCarrinho(){
         this.carrinhoService.limpar();
+    }
+
+    removerItem(rmvItem: number){
+        this.carrinhoService.removerItem(rmvItem);
     }
 }
 
